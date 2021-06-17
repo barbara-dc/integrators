cb3d4faeb9cfe0d18781c24517b3568926abd252
Barbara Bazzana <bazzana@diag.uniroma1.it>
2021-06-17 12:41:20 +0200

    shooting sebug

--- shooting_debug.m
+++ shooting_debug.m
@@ -1,10 +1,10 @@
 commandwindow
 
-s_init = [1;0];
-u_init = 0;
+s_init = [0;0;0;0];
+u_init = 1;
 t_init = 0;
 t_fin = 0.1;
-n_int = 100;
+n_int = 10;
 h = (t_fin-t_init)/n_int;
 ns = length(s_init);
 nu = length(u_init);
@@ -13,7 +13,10 @@ u = sym('u', [nu 1]);
 s = sym('s', [ns 1]);
 k = sym('k', [ns 1]);
 
-F_sym = [-16*s(1)+12*s(2)+16*cos(t)-13*sin(t)+u; 16*s(1)-9*s(2)-11*cos(t)+9*sin(t)+u];
+F_sym = [s(3); 
+    s(4); 
+    (m*l*sin(s(2))*s(4)^2 + m*gravity*cos(s(2))*sin(s(2)) + u) / (M+m-m*(cos(s(2)))^2);
+    -((m*l*cos(s(2))*sin(s(2))*s(4)^2 + (M+m)*gravity*sin(s(2)) + u*cos(s(2))) / (l*(M+m-m*(cos(s(2)))^2)))];
 dFds_sym = jacobian(F_sym, s);
 dFdu_sym = jacobian(F_sym, u);
 
@@ -48,12 +51,10 @@ matlabFunction(dsds_sym, 'vars', {t,s,u}, 'file', 'dsds');
 matlabFunction(dsdu_sym, 'vars', {t,s,u}, 'file', 'dsdu');
 
 %:)
-% [s,A,B] = expl_euler(t_init,s_init,u_init,eye(ns),zeros(ns,nu),h,n_int)
-% [x,A,B] = expl_euler_numerical_end(t_init,s_init,u_init,eye(ns),zeros(ns,nu),h,n_int)
+[s_eul,A_eul,B_eul] = expl_euler(t_init,s_init,u_init,eye(ns),zeros(ns,nu),h,n_int)
+[s_eul_num,A_eul_num,B_eul_num] = expl_euler_numerical_end(t_init,s_init,u_init,eye(ns),zeros(ns,nu),h,n_int)
 % 
- [s,A,B] = expl_rk4(t_init,s_init,u_init,eye(ns),zeros(ns,nu),h,n_int)
- [s,A,B] = expl_euler_numerical_end(t_init,s_init,u_init,eye(ns),zeros(ns,nu),h,n_int)
- 
- 
- 
- 
+ [s_,A_,B_] = expl_rk4(t_init,s_init,u_init,eye(ns),zeros(ns,nu),h,n_int)
+ [s,A,B] = expl_rk4_numerical(t_init,s_init,u_init,eye(ns),zeros(ns,nu),h,n_int)
+ tspan = [t_init t_fin];
+[t,s_ode] = ode45(@(t,s) dynamics(t,s,1), tspan, s_init);
